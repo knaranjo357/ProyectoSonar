6b138192ef56b7640cfa0a6976f0a9b355a97b15
Kevin Naranjo <knaranjo357@unab.edu.co>
2020-08-11 17:28:11 -0500

    Video 2

---
+++ ordenardataBase.m
@@ -0,0 +1,14 @@
+function sonarOrdenada = ordenardataBase(dataBase)
+sonarOrdenada(1:208,1:6)=dataBase(2:11:2288,1:6);
+sonarOrdenada(1:208,7:12)=dataBase(3:11:2288,1:6);
+sonarOrdenada(1:208,13:18)=dataBase(4:11:2288,1:6);
+sonarOrdenada(1:208,19:24)=dataBase(5:11:2288,1:6);
+sonarOrdenada(1:208,25:30)=dataBase(6:11:2288,1:6);
+sonarOrdenada(1:208,31:36)=dataBase(7:11:2288,1:6);
+sonarOrdenada(1:208,37:42)=dataBase(8:11:2288,1:6);
+sonarOrdenada(1:208,43:48)=dataBase(9:11:2288,1:6);
+sonarOrdenada(1:208,49:54)=dataBase(10:11:2288,1:6);
+sonarOrdenada(1:208,55:62)=dataBase(11:11:2288,1:8);
+
+end
+
